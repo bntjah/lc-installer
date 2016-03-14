c8bf22b4184c0712a562eca2f7c764f766b826c0
bntjah <geoffrey.vanwiele@gmail.com>
2016-03-14 14:08:23 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-echo "Copyright (C) 2016 Geoffrey 'bn_'"
-echo 
-echo "This program is free software: you can redistribute it and/or modify"
-echo "it under the terms of the GNU General Public License as published by"
-echo "the Free Software Foundation, either version 3 of the License, or"
-echo "any later version."
-echo 
-echo "This program is distributed in the hope that it will be useful,"
-echo "but WITHOUT ANY WARRANTY; without even the implied warranty of"
-echo "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-echo "GNU General Public License for more details."
-echo 
-echo "You should have received a copy of the GNU General Public License"
-echo "along with this program.  If not, see <http://www.gnu.org/licenses/>."
-echo
-
 ## Other variables
 lc_nginx_url=http://nginx.org/download/nginx-1.9.9.tar.gz
 lc_base_folder=/usr/local/lancache
