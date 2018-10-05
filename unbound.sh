1aeb648f1089f5cf5f0448f4c63927d064fee619
Geoffrey <geoffrey.vanwiele@gmail.com>
2018-10-05 15:46:38 +0200

    Update unbound.sh
    Initial work so needs testing before pushing this upstream to master

--- unbound.sh
+++ unbound.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## Original from https://github.com/zeropingheroes/lancache-dns/blob/master/install.sh#L44
 ## Used as a base so credits to them for this; I just modified the script a bith here and there
+
 # Exit if there is an error
 set -e
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # If script is executed as an unprivileged user
@@ -14,15 +14,32 @@ if [ $EUID != 0 ]; then
 fi
 
 # Prepare the upstreams config directory
-mkdir -p /etc/unbound/upstreams-available
+if [ ! -d "/etc/unbound/upstreams-available" ]; then
+	mkdir -p /etc/unbound/upstreams-available
+fi
+
+# Create Archive Folder
+if [ ! -d "/tmp/old-cache-domains/" ]; then
+	mkdir -p /tmp/old-cache-domains/
+fi
+
+# If a downloaded version exist move it to the archive folder
+if [ -d "/var/git/lancache-domains"]; then
+	mv 	/var/git/cache-domains /tmp/old-domains/$TIMESTAMP/
+fi
 
 # Get domains from `uklans/cache-domains` GitHub repo
-rm -rf /var/git/lancache-cache-domains
-/usr/bin/git clone https://github.com/uklans/cache-domains.git /var/git/lancache-cache-domains
+/usr/bin/git clone https://github.com/uklans/cache-domains.git /var/git/cache-domains/
+# Download the extra upstreams from our github
+/usr/bin/git clone -b rewrite https://github.com/bntjah/lc-installer /var/git/bntjah-installer/
+
+# Move the extra DNS from our Github into Cache-Domains folder
+if [ -d "/var/git/bntjah-installer/dns/"]; then
+	mv /var/git/bntjah-installer/dns/* /var/git/cache-domains/
+fi
 
 # Set the upstreams we want to create unbound config files from
-declare -a UPSTREAMS=("blizzard" "origin" "riot" "steam" "windowsupdates")
-#declare -a UPSTREAMS=(arena apple blizzard hirez gog glyph microsoft origin riot steam sony enmasse wargaming uplay zenimax digitalextremes pearlabyss)
+declare -a UPSTREAMS=(arena apple blizzard hirez gog glyph microsoft origin riot steam sony enmasse wargaming uplay zenimax digitalextremes pearlabyss)
 
 # Loop through each upstream file in turn
 for UPSTREAM in "${UPSTREAMS[@]}"
@@ -52,6 +69,32 @@ do
 
         # Add a standard A record config line
         echo "local-data: \"${LINE}. A $LANCACHE_IP\"" >> ${UPSTREAM_CONFIG_FILE}
-
+		
     done < /var/git/lancache-cache-domains/$UPSTREAM.txt
+done
+
+## Blacklist
+for UPSTREAM in "${UPSTREAMS[@]}"
+do
+	if [ -f "/var/git/cache-domains/${UPSTREAM}.blacklist" ]; then
+    UPSTREAM_CONFIG_FILE="/etc/unbound/upstreams-available/${UPSTREAM}.conf"
+
+    # Read the upstream file line by line
+    while read -r LINE;
+    do
+        # Skip line if it is a comment
+        if [[ ${LINE:0:1} == '#' ]]; then
+            continue
+        fi
+
+        # Check if hostname is a wildcard
+        if [[ $LINE == *"*"* ]]; then
+
+            # Remove the asterix and the dot from the start of the hostname
+            LINE=${LINE/#\*./}
+
+            # Add a wildcard config line
+            echo "local-zone: \"${LINE}.\" refuse" >> ${UPSTREAM_CONFIG_FILE}
+        fi
+    done < /var/git/lancache-cache-domains/$UPSTREAM.blacklist
 done
